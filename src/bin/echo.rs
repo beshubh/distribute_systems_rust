30e268430f68e4e41ca7ddfb27140bb402b1ca6c
beshubh <shubham.kumar@limechat.ai>
2024-08-11 14:28:35 +0530

    move init logic to lib

--- src/bin/echo.rs
+++ src/bin/echo.rs
@@ -1,4 +1,4 @@
-use anyhow::{bail, Context, Ok};
+use anyhow::{Context, Ok};
 use rustengan::*;
 use serde::{Deserialize, Serialize};
 use std::io::{StdoutLock, Write};
@@ -7,41 +7,24 @@ use std::io::{StdoutLock, Write};
 #[serde(tag = "type")]
 #[serde(rename_all = "snake_case")]
 enum Payload {
-    Init {
-        node_id: String,
-        node_ids: Vec<String>,
-    },
-    InitOk,
-    Echo {
-        echo: String,
-    },
-    EchoOk {
-        echo: String,
-    },
+    Echo { echo: String },
+    EchoOk { echo: String },
 }
 
 struct EchoNode {
     id: usize,
 }
 
-impl Node<Payload> for EchoNode {
+impl Node<(), Payload> for EchoNode {
+    fn from_init(_state: (), _init: rustengan::Init) -> anyhow::Result<Self>
+    where
+        Self: Sized,
+    {
+        Ok(EchoNode { id: 1 })
+    }
+
     fn step(&mut self, input: Message<Payload>, output: &mut StdoutLock) -> anyhow::Result<()> {
         match input.body.payload {
-            Payload::Init { .. } => {
-                let reply = Message {
-                    src: input.dst,
-                    dst: input.src,
-                    body: Body {
-                        id: Some(self.id),
-                        in_reply_to: input.body.id,
-                        payload: Payload::InitOk,
-                    },
-                };
-                serde_json::to_writer(&mut *output, &reply)
-                    .context("serialize response to init")?;
-                output.write_all(b"\n").context("write new line")?;
-                self.id += 1;
-            }
             Payload::Echo { echo } => {
                 let reply = Message {
                     src: input.dst,
@@ -57,7 +40,6 @@ impl Node<Payload> for EchoNode {
                 output.write_all(b"\n").context("write new line")?;
                 self.id += 1;
             }
-            Payload::InitOk { .. } => bail!("Unexpected InitOk message"),
             Payload::EchoOk { .. } => {}
         }
         Ok(())
@@ -65,6 +47,5 @@ impl Node<Payload> for EchoNode {
 }
 
 fn main() -> anyhow::Result<()> {
-    main_loop(EchoNode { id: 0 })?;
-    Ok(())
+    main_loop::<_, EchoNode, _>(())
 }
